a9a09ce9db68ed2142f4d74f074be90e7602f921
Christian Sosa <88207405+christian100s@users.noreply.github.com>
2023-05-08 16:49:51 -0300

    Add files via upload
    Priemra version

---
+++ HISTORIA DEL DERECHO/scripts.js
@@ -0,0 +1,80 @@
+
+/*--- JAVASCRIPT DROPDOWN "UNIVERSIDADES" ---*/
+function myAccFunc() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+
+/*--- JAVASCRIPT DROPDOWN DETRO DE LAS "UNIVERSIDADES" (U.B.A.) ---*/
+function myAccFunc1() {
+  var x = document.getElementById("demoAcc1");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+
+/*--- JAVASCRIPT DROPDOWN DETRO DE LAS "UNIVERSIDADES" (U.C.A.)---*/
+function myAccFunc2() {
+  var x = document.getElementById("demoAcc2");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+
+/*--- JAVASCRIPT DROPDOWN DETRO DE LAS "FUENTES" ---*/
+
+/*--- DROPDOWN "FUENTES" ---*/
+function myAccFunc3() {
+  var x = document.getElementById("demoAcc3");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+
+/*--- DROPDOWN "FUENTES / DIRECTAS" ---*/
+function myAccFunc4() {
+  var x = document.getElementById("demoAcc4");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+
+/*--- DROPDOWN "FUENTES / INDIRECTAS" ---*/
+function myAccFunc5() {
+  var x = document.getElementById("demoAcc5");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
